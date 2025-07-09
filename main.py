6000e608f46ac4e5f762149cabe73899440ec985
roxensox <ryanhoveskeland@gmail.com>
2025-07-08 20:13:33 -0500

    made basic one-shot functionality

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from ai-agent!")
+    if len(sys.argv) > 1:
+        prompt = " ".join(sys.argv[1:])
+    else:
+        print("Error: Please enter a prompt.")
+        sys.exit(1)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
